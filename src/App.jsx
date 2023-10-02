1ec1f8d86c76105627698dd4ed0cd69200933317
Max Pekarsky <pekarsky.maxim@gmail.com>
2023-10-02 15:53:56 -0400

    wire up getting pokemon data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import { useState } from "react";
 import Search from "./components/Search";
+import Pokemon from "./components/Pokemon";
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
+  const [pokemon, setPokemon] = useState([]);
 
-  function renderSearchResults() {
-    return <div>I'm some pokemon</div>;
+  function getDataForOnePokemon(pokemonToLookUp) {
+    // If we are already showing this pokemon, we don't want to show it again
+    // So, let's check for that upfront
+
+    // also, this is naming is awkward ¯\_(ツ)_/¯
+    // `pokemon` are all the pokemon in state.
+    // `pokemonToLookUp` is the pokemon we're gonna search for
+    // `onePokemon` is the singular of `pokemon`
+    const currentPokemonNames = pokemon.map((onePokemon) => onePokemon.name); // an array of all the pokemon names
+    if (currentPokemonNames.includes(pokemonToLookUp.name)) {
+      return;
+    }
+
+    // if we don't return above ("guard clause"), then we fetch. No need for an "else"!
+
+    fetch(pokemonToLookUp.url)
+      .then((res) => res.json())
+      .then((res) => {
+        setPokemon(pokemon.concat(res));
+      });
   }
 
   function searchForPokemon(query) {
@@ -26,6 +46,7 @@ function App() {
       .then((res) => res.json())
       .then((res) => {
         // of the 251 pokemon we get, only show ones whose names contain the query (everything lowercased)
+        // arrow functions are common for map/filter
         const matches = res.results.filter((pokemon) =>
           pokemon.name.toLowerCase().includes(query.toLowerCase())
         );
@@ -36,12 +57,71 @@ function App() {
       });
   }
 
+  function renderSearchResults() {
+    /*
+      Arrow functions are common for map/filter
+      Note the `key` attribute on the <p>.
+      Anytime we map, react need us to give every item in the list an ID (so it could re-render the list quicky)
+      Otherwise, you'll see a console error. This is a common thing to miss.
+      The key should be any unique property. In our case, every pokemon will have a different name.
+    */
+    const pokemonSearchResults = searchResults.map((pokemon) => {
+      return (
+        // note the `() => getDataForOnePokemon(pokemon.url)`
+        // if we just did `getDataForOnePokemon(pokemon.url)`, the function would run immediately, not onClick
+
+        // finally, this is gonna look like a link, even though it's not - check the CSS!
+        <p
+          key={pokemon.name}
+          onClick={() => getDataForOnePokemon(pokemon)}
+          className="search-result"
+        >
+          {pokemon.name}
+        </p>
+      );
+    });
+
+    return (
+      <div className="card">
+        <h4>Search results:</h4>
+        {/*
+          this is a common thing to do - a chunk of your component can live in its own
+          return function or variable, as long as they return JSX (which is "react HTML")
+        */}
+        {pokemonSearchResults}
+      </div>
+    );
+  }
+
+  function renderPokemon() {
+    if (!pokemon.length) {
+      return null;
+    }
+
+    /*
+      Note this pattern - we have an array of pokemon, we map over it, and render one component per array element.
+      This is super common!
+      Note that every element in a map needs a unique key attribute
+    */
+
+    const pokemonCards = pokemon.map((onePokemon) => {
+      return <Pokemon key={onePokemon.id} pokemon={onePokemon} />;
+    });
+
+    return <div className="card">{pokemonCards}</div>;
+  }
+
   return (
     <div>
       <h1 id="heading">Welcome to Pinus!</h1>
       <h2 id="subheading">(Pinus is Pokemon Linus)</h2>
       <Search placeholder="look up pokemon" handleSearch={searchForPokemon} />
+      {/*
+          this is a common thing to do - a chunk of your app can live in its own
+          return function or variable, as long as they return JSX (which is "react HTML")
+        */}
       {renderSearchResults()}
+      {renderPokemon()}
     </div>
   );
 }
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,3 +1,25 @@
-export default function Pokemon(){
-  return <p>I'm a pokemon</p>
+/*
+  Components take props objects, so you could also define this as `Pokemon(props)` where props is an object
+  that looks like {pokemon: <pokemonObject>}.
+  Then you'd call props.pokemon
+  We're `destructuring` pokemon from props - this is a common pattern
+ */
+export default function Pokemon({ pokemon }) {
+  function renderStats() {
+    return pokemon.stats.map((stats) => {
+      return (
+        <span>
+          {stats.stat.name}: {stats.base_stat}
+        </span>
+      );
+    });
+  }
+
+  return (
+    <div className="card">
+      <img src={pokemon.sprites.front_default} />
+      <h4 className="pokemon-name">{pokemon.name}</h4>
+      <div className="stats-wrapper">{renderStats()}</div>
+    </div>
+  );
 }
